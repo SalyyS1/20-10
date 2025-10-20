55a0a5c8c240b202be93c4327557f8ab19af1f62
Salyyy <121370214+SalyyS1@users.noreply.github.com>
2025-10-20 15:36:03 +0700

    Change BrowserRouter basename to root

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './styles/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/20-10/">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
