ecbb2f0ef76542a836122b3ba2be348ad8727c04
Salyyy <121370214+SalyyS1@users.noreply.github.com>
2025-10-21 23:40:44 +0700

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,4 +3,5 @@ export const SHARED_PASSWORD = 'haphuong';
 export const LETTERS: string[] = [
   "💌 Ngày 1: Chúc em một ngày 20/10 thật vui vẻ và luôn hạnh phúc. Không chỉ ngày hôm nay mà là mãi về cả sau này nhé! Nếu em rảnh mỗi ngày có thể mở một bức thư phía dưới để đọc nha, em cũng có thể trả lời bằng cách nhấp nút phản hồi phía dưới.",
   "🌅 Ngày 2: Chúc em ngày mới vuiii! Mà em đọc hết sách anh gửi chưaaa, dạo này rảnh hơn rồi nên anh cũng muốn khôi phục lại thói quen đọc sách, nếu em rảnh có thể gởi ý cho anh một số quyển sách hay với nhá!!!!. ✨",
+  "🌅 Ngày 3: Mấy quyển đó anh đọc hết lâu rồi á =)) Nói chưa đọc hết để sau có cớ nói chuyện với em thôi!!! Sách em cứ giữ đi, sau này có dịp nào anh về lại Đà Lạt thì tính tiếp. Anh đang muốn tìm thêm sách để đọc lúc rảnh á, em có đề xuất quyển nào hay không. ✨",
 ];
